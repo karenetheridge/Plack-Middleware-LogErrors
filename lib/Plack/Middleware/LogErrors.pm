f58ae36053fa432dcc6980032ceed4664f964cbf
Karen Etheridge <ether@cpan.org>
2013-06-11 16:03:35 -0700

    initial commit, via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.008, github profile.

---
+++ lib/Plack/Middleware/LogErrors.pm
@@ -0,0 +1,53 @@
+use strict;
+use warnings;
+package Plack::Middleware::LogErrors;
+# ABSTRACT: ...
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Plack::Middleware::LogErrors;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Plack-Middleware-LogErrors>
+(or L<bug-Plack-Middleware-LogErrors@rt.cpan.org|mailto:bug-Plack-Middleware-LogErrors@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,10 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use Test::Warnings;
+use Plack::Middleware::LogErrors;
+
+fail('this test is TODO!');
+
+done_testing;
