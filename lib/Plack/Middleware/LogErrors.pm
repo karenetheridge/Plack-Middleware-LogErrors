3872c8eba0f6bd11945637ac0b264b54747fe4f2
Karen Etheridge <ether@cpan.org>
2013-06-12 17:00:08 -0700

    documentation

--- lib/Plack/Middleware/LogErrors.pm
+++ lib/Plack/Middleware/LogErrors.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package Plack::Middleware::LogErrors;
-# ABSTRACT: ...
+# ABSTRACT: Map psgi.errors to psgix.logger or other logger
 
 use parent 'Plack::Middleware';
 
@@ -63,21 +63,57 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Plack::Middleware::LogErrors;
+Using a logger you have already configured (using L<Log::Dispatch> as an
+example):
 
-    ...
+    use Log::Dispatch;
+    my $logger = Log::Dispatch->new;
+    $logger->add( Log::Dispatch::File->new(...) );
+
+    builder {
+        enable 'LogDispatch', logger => $logger;
+        enable 'LogErrors';
+        $app;
+    }
+
+Using an explicitly defined logger:
+
+    builder {
+        enable 'LogErrors', logger => sub {
+            my $args = shift;
+            $logger->log(%$args);
+        };
+        $app;
+    }
 
 =head1 DESCRIPTION
 
-...
+C<psgi.errors> defaults to C<stderr> in most backends, which results in
+content going somewhere unhelpful like the server console.
 
-=head1 FUNCTIONS/METHODS
+This middleware simply remaps the C<psgi.errors> stream to the
+C<psgix.logger> stream, or an explicit logger that you provide.
+
+This is especially handy when used in combination with other middlewares
+such as L<Plack::Middleware::LogWarn> (which diverts Perl warnings to
+C<psgi.errors>);
+L<Plack::Middleware::HTTPExceptions> (which diverts
+uncaught exceptions to C<psgi.errors>);
+and L<Plack::Middleware::AccessLog>, which defaults to C<psgi.errors> when not
+passed a logger -- which is also automatically applied via L<plackup> (so if
+you provided no C<--access-log> option indicating a filename, C<psgi.errors>
+is used).
+
+=head1 CONFIGURATION
 
 =over 4
 
-=item * C<foo>
+=item * C<logger>
 
-...
+A code reference for logging messages, that conforms to the
+L<psgix.logger|PSGI::Extensions/SPECIFICATION> specification.
+If not provided, C<psgix.logger> is used, or the application will generate an
+error at runtime if there is no such logger configured.
 
 =back
 
@@ -89,15 +125,18 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Plack-Middleware-LogErrors@rt.cpan.org|mailto:bug-Plack-Middleware-LogErrors@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<PSGI::Extensions> - the definition of C<psgix.logger>
+* L<PSGI/The-Error-Stream> - the definition of C<psgi.errors>
+* L<Plack::Middleware::LogWarn> - maps warnings to C<psgi.errors>
+* L<Plack::Middleware::HTTPExceptions> - maps exceptions to C<psgi.errors>
+* L<Plack::Middleware::Log::Contextual> - use a L<Log::Contextual> logger for C<psgix.logger>
+* L<Plack::Middleware::LogDispatch> - use a L<Log::Dispatch> logger for C<psgix.logger>
+* L<Plack::Middleware::Log4perl> - use a L<Log::Log4perl> logger for C<psgix.logger>
+* L<Plack::Middleware::SimpleLogger> - essentially the opposite of this module!
 
 =end :list
 
